405cdfee2b5a4c3a93de19510e52aad32c088115
Tomas Simko <72190589+TomassSimko@users.noreply.github.com>
2023-10-01 02:26:57 +0200

    endpoints

--- api/Controller/BoxController.cs
+++ api/Controller/BoxController.cs
@@ -1,4 +1,6 @@
+using api.ActionFilter;
 using api.Dto;
+using api.Dto.Box;
 using api.Helpers;
 using Microsoft.AspNetCore.Mvc;
 using Service.Service.Interface;
@@ -27,4 +29,35 @@ public class BoxController : ControllerBase
     }
     #endregion
     
+    #region GET
+    [HttpGet]  
+    [Route("/api/boxes/{boxId}")]
+    public async Task<ResponseDto> GetAllBoxByIdAsync([FromRoute] int boxId)
+    {
+        var articles =
+            await _boxService.GetBoxByIdAsync(boxId);
+        return _response.CreateResponse(HttpContext, StatusCodeType.Success, "Successfully fetched box", articles);
+    }
+    #endregion
+
+    #region PUT
+    [HttpPut]
+    [ValidateModelFilter]
+    [Route("/api/boxes/{boxId}")]
+    public async Task<ResponseDto> UpdateBoxByIdAsync(int boxId,[FromBody] PutBoxDto boxRequest)
+    {
+        var result = await _boxService.UpdateBoxByIdAsync(boxId,boxRequest.Title,boxRequest.Type,boxRequest.Image,boxRequest.Status,boxRequest.Price,boxRequest.Color,boxRequest.Description);
+        return _response.CreateResponse(HttpContext, StatusCodeType.Success, $"Box with ID: {boxId} was updated", result);   
+    }
+    #endregion
+    
+    #region DELETE
+    [HttpDelete]
+    [Route("/api/boxes/{boxId}")]
+    public async Task<ResponseDto> DeleteBoxByIdAsync([FromRoute] int boxId)
+    {
+        var result = await _boxService.DeleteBoxByIdAsync(boxId);
+        return _response.CreateResponse(HttpContext, StatusCodeType.Created, $"Box with: {boxId} was deleted", result);
+    }
+    #endregion
 }
