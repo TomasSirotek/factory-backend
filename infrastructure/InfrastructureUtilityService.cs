d61a204964e3b112469c5ae323781a1390cc97ba
Tomas Simko <72190589+TomassSimko@users.noreply.github.com>
2023-10-03 15:17:10 +0200

    variable probem

--- infrastructure/InfrastructureUtilityService.cs
+++ infrastructure/InfrastructureUtilityService.cs
@@ -6,8 +6,6 @@ public static class InfrastructureUtilityService
 {
     
     private static readonly Uri Uri = new Uri(Environment.GetEnvironmentVariable("pgconn")!);
-   
-    
     
     public static readonly string ProperlyFormattedConnectionString =
     string.Format("Server={0};Database={1};User Id={2};Password={3};Port={4};Pooling=true;MaxPoolSize=3",
